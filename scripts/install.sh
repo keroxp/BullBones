a7a48d5fa22833626e6205dee5ec0c64861f4a59
Yusuke Sakurai <kerokerokerop@gmail.com>
2015-06-09 00:35:33 +0900

    remove scripts

--- scripts/install.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -e haxe-3.2.0 ]
-then
-  wget http://haxe.org/website-content/downloads/3.2.0/downloads/haxe-3.2.0-linux32.tar.gz
-  tar xvzf haxe-3.2.0-linux32.tar.gz
-fi
-PATH=./haxe-3.2.0:$PATH
-
